49322d25552640b659c0036f94668b25133cc91d
gram99 <76787606+gram99@users.noreply.github.com>
2026-05-05 21:13:20 -0400

    Update LoanSyndication.sol

--- LoanSyndication.sol
+++ LoanSyndication.sol
@@ -5,17 +5,34 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LoanSyndication is ERC1155, Ownable {
-    // Token IDs: 0 = Master Loan NFT, 1 = Participation Shares
+    // Token IDs
     uint256 public constant MASTER_LOAN = 0;
     uint256 public constant LOAN_SHARES = 1;
 
+    // PoC #24: Automated Monitoring Structures
+    struct MarketHealth {
+        uint256 currentMarketValue; 
+        uint256 marginThreshold;    
+        bool autoLiquidationActive; 
+    }
+    mapping(uint256 => MarketHealth) public loanHealth;
+
     constructor() ERC1155("") Ownable(msg.sender) {
-        // Mint the Master Loan NFT to the Lead Bank (you)
         _mint(msg.sender, MASTER_LOAN, 1, "");
     }
 
-    // Function to syndicate shares to partner banks
+    // Existing Syndication Logic
     function syndicateShares(address partnerBank, uint256 amount) public onlyOwner {
         _mint(partnerBank, LOAN_SHARES, amount, "");
+    }
+
+    // NEW: Automation Engine (Must be its own separate function)
+    function updateMarketValue(uint256 _loanId, uint256 _newValue) public {
+        loanHealth[_loanId].currentMarketValue = _newValue;
+        
+        // If value drops below threshold, trigger liquidation logic
+        if (_newValue < loanHealth[_loanId].marginThreshold) {
+            loanHealth[_loanId].autoLiquidationActive = true;
+        }
     }
 }
